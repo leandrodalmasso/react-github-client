e8b2dc7408b2317eae380222de64a22a940c7173
Leandro Andres Dalmasso <leandrodalmasso@gmail.com>
2019-12-16 14:54:01 -0300

    HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 // Styles
 import './styles.scss';
